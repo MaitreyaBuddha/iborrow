e2952acba700ca53f3064c09e124349fee080599
Kelly Johnson <senorkaj@gmail.com>
2015-11-11 19:14:25 -0800

    Serve static files in client folder

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(express.static('client'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
