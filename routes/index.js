ca24211f32da1d1f2ff6eec9005c2efefeb6be1c
Kelly Johnson <senorkaj@gmail.com>
2015-11-11 17:46:12 -0800

    Log into FB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+/**
+ * Author: Kelly Johnson
+ * Copyright 2015
+ * This work is licenced under Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * Main client content handler
+ */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'iBorrow' });
 });
 
 module.exports = router;
